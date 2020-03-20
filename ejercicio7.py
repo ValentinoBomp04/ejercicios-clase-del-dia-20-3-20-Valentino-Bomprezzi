52e5b9a1b0c052ba425fabc9b76e20d2270a3005
vAlentinO116912231 <61870799+vAlentinO116912231@users.noreply.github.com>
2020-03-20 11:12:42 -0300

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+import sys
+
+horas = raw_input(" Ingrese el numero de horas trabajadas: ")
+costoporhora = raw_input(" Cuanto le pagan por hora: ")
+respuesta = int(horas) * int(costoporhora)
+print "le deben pagar : " 
+print (respuesta)
