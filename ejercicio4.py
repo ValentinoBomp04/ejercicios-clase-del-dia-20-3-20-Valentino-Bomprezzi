39ba2bc2d1e316a916421d1dfec7e2932f597d8c
vAlentinO116912231 <61870799+vAlentinO116912231@users.noreply.github.com>
2020-03-20 10:19:00 -0300

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+import sys
+
+nombre = raw_input("introduzca su nombre: ")
+num = raw_input ("ingrese el numero de veces que quiera que se repita su nombre: ")  
+cuenta = (nombre * int(num))
+print (cuenta)
