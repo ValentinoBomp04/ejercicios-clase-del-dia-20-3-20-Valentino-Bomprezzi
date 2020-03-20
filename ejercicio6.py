5a731f7beadc5680117f7479e754050a4b0a206e
vAlentinO116912231 <61870799+vAlentinO116912231@users.noreply.github.com>
2020-03-20 10:59:05 -0300

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+import sys
+
+a = 2
+b = 3
+c = 4
+d = 5
+
+suma = a + b
+multiplicacion = a * d
+division = multiplicacion / suma
+resultado = (division * division)
+print "la suma es igual a"
+print (suma)
+print "la multiplicacion es igual a"
+print (multiplicacion)
+print "la division es igual a"
+print (division)
+print " el resultado es "
+print (resultado)
